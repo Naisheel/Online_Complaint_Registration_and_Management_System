70980a56d543744d6af402570c7ea65df65aa6d0
mayankg3455 <mayankg3455@gmail.com>
2023-11-17 10:06:22 +0530

    Added template file and Configured URLs and views

--- CMsystem/CMsystem/CMsystem/settings.py
+++ CMsystem/CMsystem/CMsystem/settings.py
@@ -76,8 +76,18 @@ WSGI_APPLICATION = 'CMsystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'webdata',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'Password',  
+        # Password should be same as your postgresql password
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
---
+++ CMsystem/CMsystem/web_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import include, path
+from . import views
+from django.conf import settings
+
+urlpatterns= [
+    path('',views.index,name='index')
+]
