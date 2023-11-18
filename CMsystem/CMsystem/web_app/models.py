bd1c6e84a38c02e7a648a94382505483c19d8ba0
mayankg3455 <mayankg3455@gmail.com>
2023-11-18 15:18:22 +0530

    Created Profile Model

--- CMsystem/CMsystem/web_app/models.py
+++ CMsystem/CMsystem/web_app/models.py
@@ -1,12 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
+from django.core.validators import RegexValidator
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
 
 # 1) Profile Mode
 
 # {Add Profile Model Here}
+class Meta:
+    app_label = 'CMsystem'
+class Profile(models.Model):
+    typeuser =(('student','student'),('supervisor', 'supervisor'))
+    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+    phone_regex =RegexValidator(regex=r'^\d{10,10}$', message="Phone number must be entered in the format:Up to 10 digits allowed.")
+    contact_number = models.CharField(validators=[phone_regex], max_length=10, blank=True) 
+    profile_pic = models.ImageField(null = True, blank = True)
+    type_user=models.CharField(max_length=20,default='student',choices=typeuser)
+    CB=(('ICT',"ICT"),('ICT with CS',"ICT with CS"),('MnC',"MnC"),('Robotics',"Robotics"))
+    Branch=models.CharField(choices=CB,max_length=29,default='ICT')
+    def __str__(self):
+        return self.user.username
+    
+    
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
 # 2) Complaint Model
 class Complaint(models.Model):
