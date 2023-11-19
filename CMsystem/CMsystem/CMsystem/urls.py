6e12bf4ec531a4f13fc3dd707efb287fdbd627f0
mayankg3455 <mayankg3455@gmail.com>
2023-11-19 12:10:17 +0530

    Configured Forgot Password URL

--- CMsystem/CMsystem/CMsystem/urls.py
+++ CMsystem/CMsystem/CMsystem/urls.py
@@ -20,4 +20,5 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('web_app.urls')),
+    path('accounts/',include('allauth.urls'))
 ]
--- CMsystem/CMsystem/web_app/urls.py
+++ CMsystem/CMsystem/web_app/urls.py
@@ -7,5 +7,26 @@ urlpatterns= [
     path('',views.index,name='index'),
     path('aboutus/',views.aboutus,name='aboutus'),
     path('signin/',auth_views.LoginView.as_view(template_name='CMsystem/signin.html'), name='signin'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='CMsystem/password_reset.html'
+         ),
+         name='password_reset'),
+    path('password-reset-done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='CMsystem/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='CMsystem/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='CMsystem/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+
 ]
