ce26965f8f38d8b8f705eb588a71025c68547c92
HARSHIL RAXITKUMAR PATEL <harshilraxitbhaipatel.26022002@gmail.com>
2023-12-03 00:47:52 +0530

    new testcases added

--- CMsystem/CMsystem/web_app/tests/test_models.py
+++ CMsystem/CMsystem/web_app/tests/test_models.py
@@ -18,3 +18,27 @@ class ProfileModelTest(TestCase):
     def test_profile_creation(self):
         self.assertTrue(isinstance(self.profile, Profile))
         self.assertEqual(str(self.profile), self.user.username)
+
+class ComplaintModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.complaint = Complaint.objects.create(
+            Subject='Test Subject',
+            user=self.user,
+            Type_of_complaint='Cafeteria',
+            Description='Test Description',
+            status=3
+        )
+
+    def test_complaint_creation(self):
+        self.assertTrue(isinstance(self.complaint, Complaint))
+        self.assertEqual(str(self.complaint), self.complaint.get_Type_of_complaint_display())
+
+    def test_complaint_status_change(self):
+        # Change complaint status and save
+        self.complaint.status = 1  # Solved
+        self.complaint.save()
+
+        # Retrieve the complaint from the database and check if the status is updated
+        updated_complaint = Complaint.objects.get(pk=self.complaint.pk)
+        self.assertEqual(updated_complaint.status, 1)  # 1 represents 'Solved'
