7d24b28cf58ec6af521545dfabea7a32ab1c8cc8
HARSHIL RAXITKUMAR PATEL <harshilraxitbhaipatel.26022002@gmail.com>
2023-12-03 17:29:27 +0530

    Testcase added in UserRegisterForm

--- CMsystem/CMsystem/web_app/tests/test_forms.py
+++ CMsystem/CMsystem/web_app/tests/test_forms.py
@@ -14,11 +14,9 @@ class UserRegisterFormTest(TestCase):
         }
         form = UserRegisterForm(data=form_data)
         self.assertTrue(form.is_valid())
-
     def test_invalid_user_registration_duplicate_email(self):
         # Create a user with the same email as in the form data
         User.objects.create_user(username='existinguser', email='existinguser@example.com', password='password123')
-
         form_data = {
             'username': 'newuser',
             'first_name': 'John',
@@ -30,6 +28,18 @@ class UserRegisterFormTest(TestCase):
         form = UserRegisterForm(data=form_data)
         self.assertFalse(form.is_valid())
         self.assertIn('This email address is already in use.', form.errors['email'])
+    def test_invalid_user_registration_password_mismatch(self):
+        form_data = {
+            'username': 'newuser',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'newuser@example.com',
+            'password1': 'securepassword123',
+            'password2': 'differentpassword',  # Passwords do not match
+        }
+        form = UserRegisterForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('The two password fields didn’t match.', form.errors['password2'])
 
 class UserProfileformTest(TestCase):
     def test_valid_user_profile_form(self):
@@ -39,7 +49,6 @@ class UserProfileformTest(TestCase):
         }
         form = UserProfileform(data=form_data)
         self.assertTrue(form.is_valid())
-
     def test_invalid_user_profile_form_empty_contact_number(self):
         form_data = {
             'contact_number': '',  # Empty contact number
@@ -52,7 +61,6 @@ class UserProfileformTest(TestCase):
 class ProfileUpdateFormTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='existinguser', email='existinguser@example.com', password='password123')
-
     def test_valid_profile_update_form(self):
         form_data = {
             'username': 'updateduser',
@@ -62,7 +70,6 @@ class ProfileUpdateFormTest(TestCase):
         }
         form = ProfileUpdateForm(instance=self.user, data=form_data)
         self.assertTrue(form.is_valid())
-
     def test_invalid_profile_update_form_duplicate_email(self):
         form_data = {
             'username': 'existinguser',
@@ -77,7 +84,6 @@ class ProfileUpdateFormTest(TestCase):
 class UserProfileUpdateformTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='existinguser', email='existinguser@example.com', password='password123')
-
     def test_valid_user_profile_update_form(self):
         form_data = {
             'contact_number': '9876543210',
@@ -85,7 +91,6 @@ class UserProfileUpdateformTest(TestCase):
         }
         form = UserProfileUpdateform(instance=self.user.profile, data=form_data)
         self.assertTrue(form.is_valid())
-
     def test_invalid_user_profile_update_form_empty_contact_number(self):
         form_data = {
             'contact_number': '',  # Empty contact number
@@ -104,14 +109,12 @@ class statusupdateTest(TestCase):
             Description='Test Description',
             status=3
         )
-
     def test_valid_status_update_form(self):
         form_data = {
             'status': 1,  # Solved
         }
         form = statusupdate(instance=self.complaint, data=form_data)
         self.assertTrue(form.is_valid())
-
     def test_invalid_status_update_form_invalid_status(self):
         form_data = {
             'status': 4,  # Invalid status code
@@ -129,7 +132,6 @@ class ComplaintFormTest(TestCase):
         }
         form = ComplaintForm(data=form_data)
         self.assertTrue(form.is_valid())
-
     def test_invalid_complaint_form_empty_subject(self):
         form_data = {
             'Subject': '',  # Empty subject
@@ -139,7 +141,6 @@ class ComplaintFormTest(TestCase):
         form = ComplaintForm(data=form_data)
         self.assertFalse(form.is_valid())
         self.assertIn('This field is required.', form.errors['Subject'])
-
     def test_invalid_complaint_form_long_description(self):
         form_data = {
             'Subject': 'Valid Subject',
