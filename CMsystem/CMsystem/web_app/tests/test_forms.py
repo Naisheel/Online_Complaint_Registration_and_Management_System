dcf397a0923bd2c686cb3baddfb369c8a029f1e4
HARSHIL RAXITKUMAR PATEL <harshilraxitbhaipatel.26022002@gmail.com>
2023-12-03 17:21:36 +0530

    Testcases added for ProfileUpdateForm

--- CMsystem/CMsystem/web_app/tests/test_forms.py
+++ CMsystem/CMsystem/web_app/tests/test_forms.py
@@ -48,3 +48,28 @@ class UserProfileformTest(TestCase):
         form = UserProfileform(data=form_data)
         self.assertFalse(form.is_valid())
         self.assertIn('This field is required.', form.errors['contact_number'])
+
+class ProfileUpdateFormTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='existinguser', email='existinguser@example.com', password='password123')
+
+    def test_valid_profile_update_form(self):
+        form_data = {
+            'username': 'updateduser',
+            'email': 'updateduser@example.com',
+            'first_name': 'Updated',
+            'last_name': 'User',
+        }
+        form = ProfileUpdateForm(instance=self.user, data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_profile_update_form_duplicate_email(self):
+        form_data = {
+            'username': 'existinguser',
+            'email': 'existinguser@example.com',  # This email already exists
+            'first_name': 'Updated',
+            'last_name': 'User',
+        }
+        form = ProfileUpdateForm(instance=self.user, data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('This email address is already in use.', form.errors['email'])
